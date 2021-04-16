a804584f600677ad9b33f686ac2fb5e632939826
Nimish Hathi <nhathi@sparky.fios-router.home.stsci.edu>
2021-04-16 12:40:23 -0400

    new commit

---
+++ git_update.e
@@ -0,0 +1,3 @@
+git add .
+git commit -m "pages updated"
+git push -u origin master
---
+++ other_pages/iweb_page/CV_&_Publications_files/CV_&_Publications.js
@@ -0,0 +1,9 @@
+// Created by iWeb 3.0.4 local-build-20130517
+
+setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('CV_&_Publications_files/CV_&_PublicationsMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');detectBrowser();Widget.onload();fixAllIEPNGs('Media/transparent.gif');performPostEffectsFixups()}
+function onPageUnload()
+{Widget.onunload();}
