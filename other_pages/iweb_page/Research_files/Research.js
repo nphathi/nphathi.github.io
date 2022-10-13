a830455843011b1b17fe379d6c8a16ecfe5d4f03
nphathi <nph@asu.edu>
2022-10-13 17:29:29 -0400

    pages updated

---
+++ other_pages/iweb_page/Research_files/Research.js
@@ -0,0 +1,9 @@
+// Created by iWeb 3.0.4 local-build-20130523
+
+setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('Research_files/ResearchMoz.css')
+detectBrowser();Widget.onload();fixupAllIEPNGBGs();fixAllIEPNGs('Media/transparent.gif');performPostEffectsFixups()}
+function onPageUnload()
+{Widget.onunload();}
